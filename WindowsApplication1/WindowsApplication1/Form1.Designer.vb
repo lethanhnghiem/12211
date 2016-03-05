636f46419459a1e3999e194b3557bcda000327f8
lethanhnghiem <lethanhnghiem199@gmail.com>
2016-03-04 19:30:59 -0800

    m

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtMasp = New System.Windows.Forms.TextBox()
+        Me.txtSoluong = New System.Windows.Forms.TextBox()
+        Me.txtTenSp = New System.Windows.Forms.TextBox()
+        Me.txtDonGia = New System.Windows.Forms.TextBox()
+        Me.btnThem = New System.Windows.Forms.Button()
+        Me.btnsua = New System.Windows.Forms.Button()
+        Me.btnXoa = New System.Windows.Forms.Button()
+        Me.btnLuu = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtMasp
+        '
+        Me.txtMasp.Location = New System.Drawing.Point(123, 37)
+        Me.txtMasp.Name = "txtMasp"
+        Me.txtMasp.Size = New System.Drawing.Size(100, 20)
+        Me.txtMasp.TabIndex = 0
+        '
+        'txtSoluong
+        '
+        Me.txtSoluong.Location = New System.Drawing.Point(298, 37)
+        Me.txtSoluong.Name = "txtSoluong"
+        Me.txtSoluong.Size = New System.Drawing.Size(100, 20)
+        Me.txtSoluong.TabIndex = 1
+        '
+        'txtTenSp
+        '
+        Me.txtTenSp.Location = New System.Drawing.Point(123, 80)
+        Me.txtTenSp.Name = "txtTenSp"
+        Me.txtTenSp.Size = New System.Drawing.Size(100, 20)
+        Me.txtTenSp.TabIndex = 1
+        '
+        'txtDonGia
+        '
+        Me.txtDonGia.Location = New System.Drawing.Point(298, 89)
+        Me.txtDonGia.Name = "txtDonGia"
+        Me.txtDonGia.Size = New System.Drawing.Size(100, 20)
+        Me.txtDonGia.TabIndex = 1
+        '
+        'btnThem
+        '
+        Me.btnThem.Location = New System.Drawing.Point(456, 19)
+        Me.btnThem.Name = "btnThem"
+        Me.btnThem.Size = New System.Drawing.Size(75, 54)
+        Me.btnThem.TabIndex = 2
+        Me.btnThem.Text = "them"
+        Me.btnThem.UseVisualStyleBackColor = True
+        '
+        'btnsua
+        '
+        Me.btnsua.Location = New System.Drawing.Point(456, 89)
+        Me.btnsua.Name = "btnsua"
+        Me.btnsua.Size = New System.Drawing.Size(75, 54)
+        Me.btnsua.TabIndex = 2
+        Me.btnsua.Text = "sua"
+        Me.btnsua.UseVisualStyleBackColor = True
+        '
+        'btnXoa
+        '
+        Me.btnXoa.Location = New System.Drawing.Point(576, 19)
+        Me.btnXoa.Name = "btnXoa"
+        Me.btnXoa.Size = New System.Drawing.Size(75, 54)
+        Me.btnXoa.TabIndex = 2
+        Me.btnXoa.Text = "xoa"
+        Me.btnXoa.UseVisualStyleBackColor = True
+        '
+        'btnLuu
+        '
+        Me.btnLuu.Location = New System.Drawing.Point(576, 89)
+        Me.btnLuu.Name = "btnLuu"
+        Me.btnLuu.Size = New System.Drawing.Size(75, 54)
+        Me.btnLuu.TabIndex = 2
+        Me.btnLuu.Text = "luu"
+        Me.btnLuu.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 190)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(696, 150)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(720, 352)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnLuu)
+        Me.Controls.Add(Me.btnXoa)
+        Me.Controls.Add(Me.btnsua)
+        Me.Controls.Add(Me.btnThem)
+        Me.Controls.Add(Me.txtTenSp)
+        Me.Controls.Add(Me.txtDonGia)
+        Me.Controls.Add(Me.txtSoluong)
+        Me.Controls.Add(Me.txtMasp)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtMasp As System.Windows.Forms.TextBox
+    Friend WithEvents txtSoluong As System.Windows.Forms.TextBox
+    Friend WithEvents txtTenSp As System.Windows.Forms.TextBox
+    Friend WithEvents txtDonGia As System.Windows.Forms.TextBox
+    Friend WithEvents btnThem As System.Windows.Forms.Button
+    Friend WithEvents btnsua As System.Windows.Forms.Button
+    Friend WithEvents btnXoa As System.Windows.Forms.Button
+    Friend WithEvents btnLuu As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,108 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data.DataTable
+
+Public Class Form1
+    Dim db As New DataTable
+    Dim chuoiketnoi As String = "Data Source=.;Initial Catalog=demo2;Integrated Security=True"
+    Dim conn As SqlConnection = New SqlConnection(chuoiketnoi)
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        Dim click As Integer = DataGridView1.CurrentCell.RowIndex
+        txtMasp.Text = DataGridView1.Item(0, click).Value
+        txtTenSp.Text = DataGridView1.Item(1, click).Value
+        txtSoluong.Text = DataGridView1.Item(2, click).Value
+        txtDonGia.Text = DataGridView1.Item(3, click).Value
+    End Sub
+
+    Private Sub btnThem_Click(sender As Object, e As EventArgs) Handles btnThem.Click
+        Reset()
+    End Sub
+
+    Private Sub btnsua_Click(sender As Object, e As EventArgs) Handles btnsua.Click
+        If btnsua.Text = "sua" Then
+            txtMasp.ReadOnly = True
+            btnsua.Text = "Update"
+            txtTenSp.Focus()
+        ElseIf btnsua.Text = "Update" Then
+            Dim conn As SqlConnection = New SqlConnection(chuoiketnoi)
+            Dim query As String = "update SANPHAM1 set TenSP=@TENSP, Soluong=@SOLUONG, Dongia=@DONGIA where MaSP=@MASP"
+            Dim save As SqlCommand = New SqlCommand(query, conn)
+            conn.Open()
+            save.Parameters.AddWithValue("@MASP", txtMasp.Text)
+            save.Parameters.AddWithValue("@TENSP", txtTenSp.Text)
+            save.Parameters.AddWithValue("@SOLUONG", txtSoluong.Text)
+            save.Parameters.AddWithValue("@DONGIA", txtDonGia.Text)
+            save.ExecuteNonQuery()
+            conn.Close()
+            MessageBox.Show("Update thành công")
+            txtMasp.ReadOnly = False
+            btnsua.Text = "sua"
+            LoadData()
+        End If
+    End Sub
+
+    Private Sub btnXoa_Click(sender As Object, e As EventArgs) Handles btnXoa.Click
+        If txtMasp.Text = "" Then
+            MessageBox.Show("Nhap MaSP cần xóa")
+            txtMasp.Focus()
+        Else
+            Dim delquery As String = "delete from SANPHAM1 where MaSP=@MASP"
+            Dim delete As SqlCommand = New SqlCommand(delquery, conn)
+            Dim resulft As DialogResult = MessageBox.Show("Bạn muốn xóa không?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            If resulft = Windows.Forms.DialogResult.Yes Then
+                conn.Open()
+                delete.Parameters.AddWithValue("@MASP", txtMasp.Text)
+                delete.ExecuteNonQuery()
+                conn.Close()
+                MessageBox.Show("Xóa thành công")
+                LoadData()
+            End If
+        End If
+    End Sub
+
+    Private Sub btnLuu_Click(sender As Object, e As EventArgs) Handles btnLuu.Click
+        If txtMasp.Text = "" Then
+            MessageBox.Show("Chua nhap mã sản phẩm")
+            txtMasp.Focus()
+        ElseIf txtTenSp.Text = "" Then
+            MessageBox.Show("Chua nhap Tên sản phẩm")
+            txtTenSp.Focus()
+        ElseIf txtSoluong.Text = "" Then
+            MessageBox.Show("Chua nhap Số lượng")
+            txtSoluong.Focus()
+        ElseIf txtDonGia.Text = "" Then
+            MessageBox.Show("Chua nhap đơn giá")
+            txtDonGia.Focus()
+        Else
+            Dim conn As SqlConnection = New SqlConnection(chuoiketnoi)
+            Dim query As String = "insert into SANPHAM1 values(@MASP,@TENSP,@SOLUONG,@DONGIA)"
+            Dim save As SqlCommand = New SqlCommand(query, conn)
+            conn.Open()
+            save.Parameters.AddWithValue("@MASP", txtMasp.Text)
+            save.Parameters.AddWithValue("@TENSP", txtTenSp.Text)
+            save.Parameters.AddWithValue("@SOLUONG", txtSoluong.Text)
+            save.Parameters.AddWithValue("@DONGIA", txtDonGia.Text)
+            save.ExecuteNonQuery()
+            conn.Close()
+            MessageBox.Show("Lưu thành công")
+            LoadData()
+        End If
+    End Sub
+    'load ne
+    Private Sub LoadData()
+        Dim conn As SqlConnection = New SqlConnection(chuoiketnoi)
+        conn.Open()
+        Dim refesh As SqlDataAdapter = New SqlDataAdapter("select MaSP as 'Mã SP' ,TenSP as 'Tên Sản phẩm', Soluong as 'Số lượng', Dongia as 'Đơn giá', Soluong * Dongia as 'Thành tiền' from SANPHAM1", conn)
+        db.Clear()
+        refesh.Fill(db)
+        DataGridView1.DataSource = db.DefaultView
+        conn.Close()
+    End Sub
+    ' reset cua btnthem ne
+    Private Sub reset()
+        txtDongia.Text = ""
+        txtMaSP.Text = ""
+        txtSoluong.Text = ""
+        txtTenSP.Text = ""
+        txtMaSP.Focus()
+    End Sub
+End Class
